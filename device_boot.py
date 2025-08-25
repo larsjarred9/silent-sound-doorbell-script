79d14d4d1f04120c25b7791efe4a9ccb0b073da3
Lars Jarred Speetjens <lars@speetjens.net>
2025-08-25 10:26:11 +0200

    Chore: Change API base URL to secure HTTPS endpoint

--- device_boot.py
+++ device_boot.py
@@ -16,7 +16,7 @@ except (RuntimeError, ImportError):
 
 BASE_DIR = Path("/var/silentdoorbell")
 SETTINGS_FILE = BASE_DIR / "settings.txt"
-API_BASE_URL = "http://192.168.68.114/api/devices"
+API_BASE_URL = "https://silentdoorbell.edu/api/devices"
 HEARTBEAT_INTERVAL = 180  # 3 minutes
 RETRY_INTERVAL = 60  # 1 minute
 RING_COOLDOWN = 60  # 60 seconds to prevent spamming ring events
